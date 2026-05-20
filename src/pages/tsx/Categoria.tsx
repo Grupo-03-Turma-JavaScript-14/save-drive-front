76075ba140612f2aaaa8c23c472aa1fbac54dcf8
Kauvinzera <kauvini2007@gmail.com>
2026-05-20 11:34:55 -0300

    pasta pages e components

---
+++ src/pages/tsx/Categoria.tsx
@@ -0,0 +1,24 @@
+function Categoria() {
+    return (
+        <>
+        <div className="usuarios-container">
+
+            <div className="usuarios-header">
+                <div className="titulo-area">
+                    <h1>Usuários</h1>
+
+                <p>
+                    Gerencie todos os usuários cadastrados na plataforma
+                </p>
+                </div>
+                <button className="btn-criar"> 
+                    + Novo Usuário
+                </button>
+            </div>
+        </div>
+        </>
+    )
+
+}
+
+export default Categoria
