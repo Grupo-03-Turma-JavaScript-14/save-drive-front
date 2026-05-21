57e2e314e8d8bf4af057bc01eafae2e8889f0ff7
JhonatanMiranda221 <jhonatanmiranda221@gmail.com>
2026-05-21 09:43:53 -0300

    imagens

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AboutUs from './pages/tsx/Aboutus'
 
 function App() {
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sobre" element={<AboutUs />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
